9f77d6f50eb2c2d533c77e4da6fdfd95f7da49fe
trandung2k1 <trandungitksnb2001@gmail.com>
2023-05-28 00:16:02 +0700

    update lesson01

--- lesson01/src/main.ts
+++ lesson01/src/main.ts
@@ -7,3 +7,10 @@ console.log(PI);
 const a: number = 12;
 const b: number = 6;
 console.log(a / b);
+const isLoading: boolean = true;
+console.log(isLoading);
+const cars: string[] = ['Mercedes', 'BMW', 'Volvo', 'Audi'];
+console.log(cars);
+//Check type variable
+const typeOf = (value: any) => Object.prototype.toString.call(value).slice(8, -1);
+console.log(typeOf(cars));
---
+++ lesson01/src/sum.ts
@@ -0,0 +1,5 @@
+function sum(a: number, b: number): number {
+    return a + b;
+}
+const rs: number = sum(1, 2);
+console.log(rs);
