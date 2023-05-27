2e8cfce3352f2758336697b9301b71d520b00cbf
trandung2k1 <trandungitksnb2001@gmail.com>
2023-05-28 00:06:24 +0700

    add lesson01

---
+++ lesson01/README.md
@@ -0,0 +1,17 @@
+1. Installl typescript global
+
+```js
+npm install typescript -g
+```
+
+2. Check version
+
+```js
+tsc -v
+```
+
+3. Run watch
+
+```js
+tsc -w
+```
---
+++ lesson01/src/main.ts
@@ -0,0 +1,9 @@
+let fullName: string = 'Dung';
+console.log(fullName);
+const age: number = 21;
+console.log(age);
+const PI: number = 3.14;
+console.log(PI);
+const a: number = 12;
+const b: number = 6;
+console.log(a / b);
